d5d0eba360945b5a59cfaa53fc9397d434a42f59
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-08-16 19:52:00 -0700

    Conexión BD
    Conexión a Base de Datos del sistemas.

---
+++ EventosSociales/src/persistencia/IConexionBD.java
@@ -0,0 +1,19 @@
+package persistencia;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Conexion Clase.
+ *
+ */
+public interface IConexionBD {
+
+    /**
+     * Crear conexion a la base de datos.
+     *
+     * @return La conexion creada
+     * @throws SQLException Si esta no se puede conectar.
+     */
+    public Connection crearConexion() throws SQLException;
+}
