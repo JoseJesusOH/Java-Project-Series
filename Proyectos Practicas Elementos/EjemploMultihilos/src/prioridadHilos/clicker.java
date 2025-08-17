a852505337bda041958414955d6a412837e104e6
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-08-16 20:21:14 -0700

    Implementación Hilos 2
    2 implementación para hilos.

---
+++ Proyectos Practicas Elementos/EjemploMultihilos/src/prioridadHilos/clicker.java
@@ -0,0 +1,31 @@
+
+package prioridadHilos;
+
+
+public class clicker implements Runnable {
+
+    int click = 0;
+    Thread t;
+    private volatile boolean running = true;
+
+    public clicker(int p) {
+        t = new Thread(this);
+        t.setPriority(p);
+    }
+
+    @Override
+    public void run() {
+        while (running) {
+            click++;
+        }
+    }
+
+    public void stop() {
+        running = false;
+    }
+
+    public void start() {
+        t.start();
+    }
+
+}
