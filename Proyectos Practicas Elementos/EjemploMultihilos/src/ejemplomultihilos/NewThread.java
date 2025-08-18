289667807da535b1e1af28797dd86477172d58af
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-08-17 21:09:33 -0700

    Implementación 4 Hilos Thread
    Clase para la implementación de Hilos en conjunto del método sleep.

---
+++ Proyectos Practicas Elementos/EjemploMultihilos/src/ejemplomultihilos/NewThread.java
@@ -0,0 +1,31 @@
+
+package ejemplomultihilos;
+
+
+public class NewThread implements Runnable {
+
+    String name; // nombre del hilo
+    Thread t;
+
+    public NewThread(String threadname) {
+        name = threadname;
+        t = new Thread(this, name);
+        System.out.println("Nuevo hilo: " + t);
+        t.start(); // Comienza el hilo
+    }
+    // Este es el punto de entrada del hilo.
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 5; i > 0; i--) {
+                System.out.println(name + ": " + i);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println(name + "Interrupción del hilo hijo" + name);
+        }
+        System.out.println("Sale del hilo hijo" + name);
+    }
+
+}
