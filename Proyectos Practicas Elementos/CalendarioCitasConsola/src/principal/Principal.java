84edee5166fd315a25d1aaf67c710a5b2cdaeeaf
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-08-24 17:27:12 -0700

    Lanzador Principal - Java
    Clase que ejecuta la aplicación del calendario de citas.

---
+++ Proyectos Practicas Elementos/CalendarioCitasConsola/src/principal/Principal.java
@@ -0,0 +1,15 @@
+package principal;
+
+
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Menu miMenu = new Menu();
+        miMenu.desplegarMenu();
+    }
+    
+}
