f453d1c51bb5233947b63b6d5d074456e16c258f
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-08-24 17:26:02 -0700

    Cita Calendario - Java
    Clase representativa de la cita donde se incluyen los datos mas comunes como lo es el nombre del estudiante, maestro, tipo clase y tipo de pago.

---
+++ Proyectos Practicas Elementos/CalendarioCitasConsola/src/principal/Cita.java
@@ -0,0 +1,50 @@
+package principal;
+
+
+public class Cita {
+    private String nombreEstudiante, maestro, tipoClase, tipoPago;
+
+    public Cita() {
+    }
+
+    public Cita(String nombreEstudiante, String maestro, String tipoClase, String tipoPago) {
+        this.nombreEstudiante = nombreEstudiante;
+        this.maestro = maestro;
+        this.tipoClase = tipoClase;
+        this.tipoPago = tipoPago;
+    }
+
+    public String getNombreEstudiante() {
+        return nombreEstudiante;
+    }
+
+    public void setNombreEstudiante(String nombreEstudiante) {
+        this.nombreEstudiante = nombreEstudiante;
+    }
+
+    public String getMaestro() {
+        return maestro;
+    }
+
+    public void setMaestro(String maestro) {
+        this.maestro = maestro;
+    }
+
+    public String getTipoClase() {
+        return tipoClase;
+    }
+
+    public void setTipoClase(String tipoClase) {
+        this.tipoClase = tipoClase;
+    }
+
+    public String getTipoPago() {
+        return tipoPago;
+    }
+
+    public void setTipoPago(String tipoPago) {
+        this.tipoPago = tipoPago;
+    }
+    
+    
+}
